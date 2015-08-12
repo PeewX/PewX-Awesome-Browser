b42f387e6f47a5457aa352ce3bab36a74a97d748
HorrorClown <admin@pewx.de>
2015-08-12 14:34:23 +0200

    Browser Manager, Core and definitions update

---
+++ PAB/client/classes/CBrowser/CBrowserManager.lua
@@ -0,0 +1,44 @@
+--
+-- HorrorClown (PewX)
+-- Using: IntelliJ IDEA 14 Ultimate
+-- Date: 26.07.2015 - Time: 04:18
+-- pewx.de // iGaming-mta.de // iRace-mta.de // iSurvival.de // mtasa.de
+--
+CBrowserManager = {}
+
+function CBrowserManager:constructor()
+
+    self.sConfigPath = "res/config/browserDefinitions.xml"
+    self:loadBrowserDefinitions()
+end
+
+function CBrowserManager:destructor()
+
+end
+
+function CBrowserManager:initBrowserDefinitions()
+    local xml = XML.load(self.sConfigPath)
+    if not xml then return end
+
+    --Main browser definitions
+    local eConfigNode = xml:findChild("config", 0)
+    self.tBrowserDefinitions = {}
+
+    for _, eChild in ipairs(eConfigNode:getChildren()) do
+        self.tBrowserDefinitions[eChild:getName()] = eChild:getValue()
+    end
+
+    --Color definitions
+    local eColorsNode = xml:findChild("colors", 0)
+    self.tColorDefinitions = {}
+
+    for _, eMasterChild in ipairs(eColorsNode:getChildren()) do
+        self.tColorDefinitions[eMasterChild] = {}
+
+        for _, eChild in ipairs(eMasterChild:getChildren()) do
+           self.tColorDefinitions[eChild:getName()] = eChild:getValue()
+       end
+    end
+
+    self.bDefinitionsLoaded = true
+end
---
+++ PAB/client/system/CCore.lua
@@ -0,0 +1,45 @@
+--
+-- HorrorClown (PewX)
+-- Using: IntelliJ IDEA 14 Ultimate
+-- Date: 19.12.2014 - Time: 18:41
+-- pewx.de // iGaming-mta.de // iRace-mta.de // iSurvival.de // mtasa.de
+--
+CCore = {}
+
+function CCore:constructor()
+    self.managers = {}
+
+    ---Manager Table: {"ManagerName", {arguments}}
+    table.insert(self.managers, {"CDXManager", {}})
+    table.insert(self.managers, {"CBrowserManager", {}})
+end
+
+function CCore:destructor()
+
+end
+
+function CCore:getManager(sName)
+    return self[sName]
+end
+
+function CCore:startScript()
+    for _, v in ipairs(self.managers) do
+        if (type(_G[v[1]]) == "table") then
+            self[tostring(v[1])] = new(_G[v[1]], unpack(v[2]))
+            debugOutput(("[CCore] Loading manager '%s'"):format(tostring(v[1])))
+        else
+            debugOutput(("[CCore] Couldn't find manager '%s'"):format(tostring(v[1])))
+        end
+    end
+end
+
+--Create the Core Instance
+addEventHandler("onClientResourceStart", resourceRoot,
+    function()
+        local s = getTickCount()
+        debugOutput("[CCore] Starting iGaming")
+        Core = new(CCore)
+        Core:startScript()
+        debugOutput(("[CCore] Starting finished (%sms)"):format(getTickCount()-s))
+    end
+)
